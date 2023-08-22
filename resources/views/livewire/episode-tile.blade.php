600dd8dc606cbd9dcd54a8f34559323dda588efa
gringo.dev <jon@gringo.dev>
2023-08-22 10:56:39 -0600

    Simplify episode tile Add real published date If people don't use reason to guide their souls, they're doomed to unhappiness.

--- resources/views/livewire/episode-tile.blade.php
+++ resources/views/livewire/episode-tile.blade.php
@@ -4,35 +4,25 @@ use function Livewire\Volt\{state, computed};
 
 /** @var \App\Models\Episode $episode */
 state('episode')->reactive();
-state(['expanded' => false, 'transcriptTeaserLength' => 200]);
-
-$exceedsTeaserLength = computed(function () {
-    return strlen($this->episode->transcript) > $this->transcriptTeaserLength;
-});
+state(['expanded' => false]);
 
 $downloadFile = fn() => Storage::disk('public')->download($this->episode->file_path);
 
 ?>
 
 <div class="flex flex-col gap-4 w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg mb-6">
-    <p class="font-extralight text-gray-400 text-sm">Feb 24 2022</p>
+    <p class="font-extralight text-gray-400 text-sm">{{ $episode->updated_at->toFormattedDateString() }}</p>
     <h1 class="text-lg font-semibold">
         {{$episode->title}}
     </h1>
     <audio controls src="{{ Storage::disk('public')->url($episode->file_path) }}"></audio>
-    <p>
-        <span x-show="$wire.expanded">{{$episode->transcript}}</span>
-        <span x-show="!$wire.expanded" @class(["after:content-['...']" => $this->exceedsTeaserLength])>
-            {{substr($episode->transcript, 0, $transcriptTeaserLength)}}
-        </span>
+    <p class="truncate" :class="{ 'truncate': !$wire.expanded }">
+        <span>{{$episode->transcript}}</span>
     </p>
     <div>
-        @if($this->exceedsTeaserLength)
-            <x-primary-button x-on:click="$wire.expanded = !$wire.expanded">
-                <span x-show="$wire.expanded">Show Less</span>
-                <span x-show="!$wire.expanded">Show More</span>
-            </x-primary-button>
-        @endif
+        <x-primary-button x-on:click="$wire.expanded = !$wire.expanded">
+            <span x-text="$wire.expanded ? 'Show Less' : 'Show More'">Show More</span>
+        </x-primary-button>
         <x-secondary-button wire:click="downloadFile">
             Download
         </x-secondary-button>
