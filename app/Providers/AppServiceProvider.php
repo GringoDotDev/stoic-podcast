8856a3a1541b4c0fcb24b7d9a362e860d9fa99dc
gringo.dev <jon@gringo.dev>
2023-11-19 14:17:01 -0600

    Force https in production
    Waste no more time arguing what a good man should be. Be One.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::shouldBeStrict(!$this->app->isProduction());
+
+        // force https in prod
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
